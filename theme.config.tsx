f5a9f282e2de2cf22dce0622a47565a9eea648ae
rainboowbee <rainboowbee@gmail.com>
2025-02-28 21:17:51 +0300

    first commit

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>BitLab</span>,
+  project: {
+    link: "https://github.com/rainboowbee/next-learn-platform.git",
+  },
+  docsRepositoryBase: "https://github.com/rainboowbee/next-learn-platform.git",
+  footer: {
+    text: "BitLab",
+  },
+};
+
+export default config;
